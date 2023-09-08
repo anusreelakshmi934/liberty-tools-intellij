1685200ed986504800dc96897bf0cfb75537b10b
anusreelakshmi934 <anusree.lakshmi@ibm.com>
2023-09-08 15:44:53 +0530

    Fixes #196
    Fixes#196. Created Error popup if Java 17  is not found to launch language servers

--- src/main/java/io/openliberty/tools/intellij/liberty/lsp/LibertyXmlServer.java
+++ src/main/java/io/openliberty/tools/intellij/liberty/lsp/LibertyXmlServer.java
@@ -15,9 +15,11 @@ import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.openapi.extensions.PluginId;
 import io.openliberty.tools.intellij.lsp4mp.lsp4ij.server.ProcessStreamConnectionProvider;
 import io.openliberty.tools.intellij.util.Constants;
+import io.openliberty.tools.intellij.util.LocalizedResourceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
 import java.io.File;
 import java.net.URI;
 import java.util.ArrayList;
@@ -32,21 +34,11 @@ public class LibertyXmlServer extends ProcessStreamConnectionProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(LibertyXmlServer.class);
 
     public LibertyXmlServer() {
+        super("LibertyXmlServer");
         IdeaPluginDescriptor descriptor = PluginManagerCore.getPlugin(PluginId.getId("open-liberty.intellij"));
         File lemminxServerPath = new File(descriptor.getPluginPath().toFile(), "lib/server/org.eclipse.lemminx-uber.jar");
         File libertyServerPath = new File(descriptor.getPluginPath().toFile(), "lib/server/liberty-langserver-lemminx-2.0-jar-with-dependencies.jar");
         String javaHome = System.getProperty("java.home");
-        if (javaHome == null) {
-            LOGGER.error("Unable to launch the LemMinX language server. Could not resolve the java home system property");
-            return;
-        }
-        if (!checkJavaVersion(javaHome, Constants.REQUIRED_JAVA_VERSION)) {
-            LOGGER.error("Unable to launch the LemMinX language server." +
-                    " Java " + Constants.REQUIRED_JAVA_VERSION + " or more recent is required to run 'Liberty Tools for IntelliJ'." +
-                    " Change the boot Java runtime of the IDE as documented here:" +
-                    " https://www.jetbrains.com/help/idea/switching-boot-jdk.html");
-            return;
-        }
         if (lemminxServerPath.exists() && libertyServerPath.exists()) {
             ArrayList<String> params = new ArrayList<>();
             params.add(javaHome + File.separator + "bin" + File.separator + "java");
