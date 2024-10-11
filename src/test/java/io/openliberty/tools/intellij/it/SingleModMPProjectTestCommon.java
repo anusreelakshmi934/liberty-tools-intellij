c002de08fd0b2521af84bca0dc53c466e4cb8584
Anusree Lakshmi <141303561+anusreelakshmi934@users.noreply.github.com>
2024-10-11 09:56:14 +0530

    Stop server

--- src/test/java/io/openliberty/tools/intellij/it/SingleModMPProjectTestCommon.java
+++ src/test/java/io/openliberty/tools/intellij/it/SingleModMPProjectTestCommon.java
@@ -63,8 +63,8 @@ public abstract class SingleModMPProjectTestCommon {
     @AfterEach
     public void afterEach(TestInfo info) {
         TestUtils.printTrace(TestUtils.TraceSevLevel.INFO, this.getClass().getSimpleName() + "." + info.getDisplayName() + ". Exit");
-        UIBotTestUtils.killTerminalProcess(remoteRobot, getSmMPProjectName() );
         cleanupTerminal();
+        UIBotTestUtils.killTerminalProcess(remoteRobot, getSmMPProjectName() );
     }
 
     /**
